6e677b8568336375b3a54d14c874a3cadbc96e90
Sebastian Ruchlewicz <sebastian.ruchlewicz@gmail.com>
2018-08-13 23:09:30 +0200

    init commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    Ruchlewicz
+ * @package     Ruchlewicz_Sample
+ * @author      Sebastian Ruchlewicz <sebastian.ruchlewicz@gmail.com>
+ * @copyright   Copyright (c) Sebastian Ruchlewicz (https://ruchlewicz.net/)
+ * @license     https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ruchlewicz_Sample',
+    __DIR__
+);
